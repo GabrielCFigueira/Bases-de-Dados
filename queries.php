8bd0cf05f6c35105d47af7f1c71cb58d90ab8b53
Grabiel14 <33735410+Grabiel14@users.noreply.github.com>
2018-11-23 19:49:05 +0000

    Add some more insert/update/remove queries
    +Add selection queries

--- queries.php
+++ queries.php
@@ -5,6 +5,7 @@
     <body>
 <?php
 
+    //insert queries
     $insertLocal = "insert into Local(moradaLocal) values(:moradaLocal)";
     $insertEvento = "insert into EventoEmergencia(numTelefone, instanteChamada, nomePessoa, 
 moradaLocal, numProcessoSocorro) values(:numTelefone, :instanteChamada, :nomePessoa, 
@@ -13,14 +14,52 @@ moradaLocal, numProcessoSocorro) values(:numTelefone, :instanteChamada, :nomePes
     $insertMeio = "insert into Meio(numMeio, nomeMeio, nomeEntidade) values(:numMeio, :nomeMeio, 
 :nomeEntidade)";
     $insertEntidade = "insert into EntidadeMeio(nomeEntidade) values(:nomeEntidade=?)";
+    $insertMeioCombate = "insert into MeioCombate(numMeio, nomeEntidade) values(:numMeio, :nomeEntidade);"
+    $insertMeioApoio = "insert into MeioApoio(numMeio, nomeEntidade) values(:numMeio, :nomeEntidade);"
+    $insertMeioSocorro = "insert into MeioSocorro(numMeio, nomeEntidade) values(:numMeio, :nomeEntidade);"
 
+
+    
+    //remove queries
     $removeLocal = "delete from Local where moradaLocal= :moradaLocal";
     $removeEvento = "delete from EventoEmergencia where numTelefone = :numTelefone and 
 instanteChamada = :instanteChamada";
     $removeProcesso = "delete from ProcessoSocorro where numProcessoSocorro = :numProcessoSocorro";
     $removeMeio = "delete from Meio where numMeio = :numMeio and nomeEntidade = :nomeEntidade";
     $removeEntidade = "delete from EntidadeMeio where nomeEntidade = :nomeEntidade";
+    $removeMeioCombate = "delete from MeioCombate where numMeio = :numMeio and nomeEntidade = :nomeEntidade"
+    $removeMeioApoio = "delete from MeioApoio where numMeio = :numMeio and nomeEntidade = :nomeEntidade"
+    $removeMeioSocorro = "delete from MeioSocorro where numMeio = :numMeio and nomeEntidade = :nomeEntidade"
 
+
+
+    //update queries
+    $updateMeioCombate = "update MeioCombate set numMeio = :newnumMeio and nomeEntidade = :newnomeEntidade
+where numMeio = :oldnumMeio and nomeEntidade = :oldnomeEntidade"
+    $updateMeioApoio = "update MeioApoio set numMeio = :newnumMeio and nomeEntidade = :newnomeEntidade
+where numMeio = :oldnumMeio and nomeEntidade = :oldnomeEntidade"
+    $updateMeioSocorro = "update MeioSocorro set numMeio = :newnumMeio and nomeEntidade = :newnomeEntidade
+where numMeio = :oldnumMeio and nomeEntidade = :oldnomeEntidade"
+
+
+
+    //listar Processos de Socorro
+    $listProcessoSocorro = "select * from ProcessoSocorro";
+
+    //listar Meios
+    $listMeio = "select * from Meio";
+
+    //associar Processos de Socorro a Meios
+    $assocProcessoMeio = null;
+
+    //associar Processos de Socorro a Eventos de Emergência
+    $assocProcessoEvento = null;
+
+    //listar os Meios accionados num Processo de Socorro
+    $accioProcessoMeio = null;
+
+    //listar os Meios de Socorro accionados em processos de Socorros originados num dado local de incendio
+    $accioMeioSocorroLocal = null;
 
     try 
     {
