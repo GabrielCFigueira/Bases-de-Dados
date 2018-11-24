63c1781bb72c78174f464abbb1fb71e18f68cb55
Grabiel14 <33735410+Grabiel14@users.noreply.github.com>
2018-11-24 21:20:05 +0000

    Pass the variable "table" throught hidden input  HEAD detached at 9e830c2  Changes to be committed: 	modified:   Insert_Queries/insertLocal.php 	modified:   index.html

--- insert.php
+++ insert.php
@@ -10,7 +10,7 @@
     if ($table == "Local") {
 
         echo("<form action='Insert_Queries/insertLocal.php' method='post'>
-        <p><input type='hidden' name='table' value='<?=$_REQUEST['table']?>'/></p>
+        <p><input type='hidden' name='table' value='$table'/></p>
         <p>Inserir novo Local</p>
         <p>Morada do Local: <input type='text' name='moradaLocal'/>
         <input type='submit' value='Submit'/></p>
@@ -19,7 +19,7 @@
     } else  if ($table == "Evento") {
 
         echo("<form action='Insert_Queries/insertLocal.php' method='post'>
-        <p><input type='hidden' name='table' value='<?=$_REQUEST['table']?>'/></p>
+        <p><input type='hidden' name='table' value='$table'/></p>
         <p>Inserir novo Evento de Emergência</p>
         <p>Número de Telefone: <input type='text' name='numTelefone'/>
         <p>Instante da Chamada: <input type='text' name='instanteChamada'/>
@@ -32,7 +32,7 @@
     } else  if ($table == "Processo") {
 
         echo("<form action='Insert_Queries/insertLocal.php' method='post'>
-        <p><input type='hidden' name='table' value='<?=$_REQUEST['table']?>'/></p>
+        <p><input type='hidden' name='table' value='$table'/></p>
         <p>Inserir novo Processo de Socorro</p>
         <p>Número de Processo Socorro: <input type='text' name='numProcessoSocorro'/>
         <input type='submit' value='Submit'/></p>
@@ -41,7 +41,7 @@
     } else  if ($table == "Meio") {
     
         echo("<form action='Insert_Queries/insertLocal.php' method='post'>
-        <p><input type='hidden' name='table' value='<?=$_REQUEST['table']?>'/></p>
+        <p><input type='hidden' name='table' value='$table'/></p>
         <p>Inserir novo Meio</p>
         <p>Número do Meio: <input type='text' name='numMeio'/>
         <p>Nome do Meio: <input type='text' name='nomeMeio'/>
@@ -52,7 +52,7 @@
     } else  if ($table == "Entidade") {
 
         echo("<form action='Insert_Queries/insertLocal.php' method='post'>
-        <p><input type='hidden' name='table' value='<?=$_REQUEST['table']?>'/></p>
+        <p><input type='hidden' name='table' value='$table'/></p>
         <p>Inserir nova Entidade</p>
         <p>Nome da Entidade : <input type='text' name='nomeEntidade'/>
         <input type='submit' value='Submit'/></p>
@@ -61,7 +61,7 @@
     } else  if ($table == "MeioCombate") {
 
         echo("<form action='Insert_Queries/insertLocal.php' method='post'>
-        <p><input type='hidden' name='table' value='<?=$_REQUEST['table']?>'/></p>
+        <p><input type='hidden' name='table' value='$table'/></p>
         <p>Inserir novo Meio de Combate</p>
         <p>Número do Meio: <input type='text' name='numMeio'/>
         <p>Nome do Meio: <input type='text' name='nomeMeio'/>
@@ -71,7 +71,7 @@
     } else  if ($table == "MeioApoio") {
 
         echo("<form action='Insert_Queries/insertLocal.php' method='post'>
-        <p><input type='hidden' name='table' value='<?=$_REQUEST['table']?>'/></p>
+        <p><input type='hidden' name='table' value='$table'/></p>
         <p>Inserir novo Meio de Apoio</p>
         <p>Número do Meio: <input type='text' name='numMeio'/>
         <p>Nome do Meio: <input type='text' name='nomeMeio'/>
@@ -81,7 +81,7 @@
     } else  if ($table == "MeioSocorro") {
 
         echo("<form action='Insert_Queries/insertLocal.php' method='post'>
-        <p><input type='hidden' name='table' value='<?=$_REQUEST['table']?>'/></p>
+        <p><input type='hidden' name='table' value='$table'/></p>
         <p>Inserir novo Meio de Socorro</p>
         <p>Número do Meio: <input type='text' name='numMeio'/>
         <p>Nome do Meio: <input type='text' name='nomeMeio'/>
