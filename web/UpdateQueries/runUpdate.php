684769f79b42fd2d446e0e1a14de250ae14e1321
PinoxMeister <pedro.matias.carvalho@gmail.com>
2018-11-27 22:42:35 +0000

    .

--- web/UpdateQueries/runUpdate.php
+++ web/UpdateQueries/runUpdate.php
@@ -176,13 +176,13 @@ catch (PDOException $e)
 {    
     switch($e->getCode()){
     case 23505:
-        echo("Chave duplicada. O elemento nao foi inserido");
+        echo("<p id='error'>Chave duplicada. O elemento nao foi inserido</p>");
         break;
     case 23503:
-        echo("Chave estrangeira inexistente");
+        echo("<p id='error'>Chave estrangeira inexistente</p>");
         break;
     case 22P02:
-        echo("Campo inválido");
+        echo("<p id='error'>Campo inválido</p>");
         break;
 }
     //echo("<p>ERROR: {$e->getMessage()}</p>");
